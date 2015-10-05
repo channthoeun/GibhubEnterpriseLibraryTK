4d1cacb4a60a6166914188261dbae27ae75db3d3
Channthoeun Ken <channthoeun@gmail.com>
2015-10-05 20:03:02 +0700

    Data Access Application Block

--- GithubEnterpriseLibraryTK/Product.aspx
+++ GithubEnterpriseLibraryTK/Product.aspx
@@ -1,3 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Product.aspx.cs" Inherits="GithubEnterpriseLibraryTK.Product" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Products List</h2>
+    <asp:GridView ID="grvProduct" runat="server"></asp:GridView>
 </asp:Content>
--- GithubEnterpriseLibraryTK/Product.aspx.cs
+++ GithubEnterpriseLibraryTK/Product.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data;
 
 namespace GithubEnterpriseLibraryTK
 {
@@ -11,6 +13,32 @@ namespace GithubEnterpriseLibraryTK
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (! Page.IsPostBack)
+            {
+                LoadProducts();
+            }
+        }
+
+        private void LoadProducts()
+        {
+            //This code is used to call stored procedure without parameter and bind gridview
+            DatabaseProviderFactory factory = new DatabaseProviderFactory();
+            Database objDataBase = factory.Create("Sales"); 
+            DataSet ds = new DataSet();
+            try
+            {
+                ds = objDataBase.ExecuteDataSet(CommandType.StoredProcedure, 
+                                                    "roducts_GetProducts");
+                // ds = objDataBase.ExecuteDataSet("SELECT * FROM tblProducts;");
+                // ds = objDataBase.ExecuteDataSet(CommandType.Text, sql);
+                grvProduct.DataSource = ds;
+                grvProduct.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+
+            }
 
         }
     }
