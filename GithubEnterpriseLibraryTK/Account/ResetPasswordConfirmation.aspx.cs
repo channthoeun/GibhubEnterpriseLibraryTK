13837343912c989f0c1be70421c17867f11ea809
Channthoeun Ken <channthoeun@gmail.com>
2015-10-05 18:32:57 +0700

    First Commit

---
+++ GithubEnterpriseLibraryTK/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GithubEnterpriseLibraryTK.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ GithubEnterpriseLibraryTK/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GithubEnterpriseLibraryTK.Startup))]
+namespace GithubEnterpriseLibraryTK
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
